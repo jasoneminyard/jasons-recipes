ed9243d5bd62f5676e2dda9065df9dbedad0d124
jasoneminyard <jason.e.minyard@gmail.com>
2024-07-11 15:56:01 -0500

    add test for homepage_controller

--- test/controllers/homepage_controller_test.rb
+++ test/controllers/homepage_controller_test.rb
@@ -4,4 +4,12 @@ class HomepageControllerTest < ActionDispatch::IntegrationTest
   setup do
     @recipe = recipes(:one)
   end
+
+  test "should get index" do # this just passes thru to empty erb files
+    get root_url
+    assert_response :success # all responses are json and :success
+
+    get "/*path"
+    assert_response :success # all responses are json and :success
+  end
 end
