439b999e911b046195ef66c549b63af9884a5cae
jasoneminyard <jason.e.minyard@gmail.com>
2024-07-24 11:28:22 -0500

    add image realation

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,7 @@
 class Recipe < ApplicationRecord
-    validates :name, presence: true
-    validates :ingredients, presence: true
-    validates :instruction, presence: true
-  end
+  has_one_attached :image
+
+  validates :name, presence: true
+  validates :ingredients, presence: true
+  validates :instruction, presence: true
+end
