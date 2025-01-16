0c13a3792ebf3fd1e9b8672f13e13cb8fb11ea40
jasoneminyard <jason.e.minyard@gmail.com>
2025-01-16 11:00:38 -0600

    cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -5,31 +5,7 @@ Rails.application.routes.draw do
     end
   end
 
-  # # Mount ActiveStorage routes
-  # mount ActiveStorage::Engine => '/rails/active_storage'
-
-  # # Include ActiveStorage routes explicitly
-  # direct :rails_blob do |model|
-  #   ActiveStorage::Blob.routes(model)
-  # end
-
-  # direct :rails_blob do |blob|
-  #   route_for(:rails_blob, blob)
-  # end
-  # direct :rails_blob_proxy do |blob|
-  #   route_for(:rails_blob_proxy, blob)
-  # end
-  # direct :rails_disk_service do |disk|
-  #   route_for(:rails_disk_service, disk)
-  # end
-
-  # direct :rails_blob_proxy_service do |proxy|
-  #   route_for(:rails_blob_proxy_service, proxy)
-  # end
-
-  # # Define ActiveStorage routes here if not already included
-  # get '/rails/active_storage/blobs/redirect/*path', to: 'active_storage/blobs#redirect'
-  # get '/rails/active_storage/blobs/proxy/*path', to: 'active_storage/blobs#proxy'
+  root "homepage#index"
 
   # Catch-all route (ensure it's at the bottom)
   get '*path', to: 'homepage#index', constraints: lambda { |req|
